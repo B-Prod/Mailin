1da151b5df0849a38bd556a09b966378449ccd11
B-Prod <fabien@bpcn-server-01.(none)>
2013-04-18 01:12:10 +0200

    Fix the awaiting issues thanks to the Mailin team API upgrade. Update the Unit tests.

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 Mailin
 ======
 
-Provides integration with Mailin, an email delivery service.
+Provides integration with [Mailin](http://www.mailin.fr/), an email delivery service.
+
+Requirements
+--------------------
+
+<dl>
+  <dt>Minimum PHP version</dt>
+  <dd>5.3</dd>
+  <dt>Mailin account</dt>
+  <dd>You can create one for free</dd>
+</dl>
+
--- Tests/MailinAPITest.php
+++ Tests/MailinAPITest.php
@@ -46,17 +46,47 @@ class MailinAPITest extends \PHPUnit_Framework_TestCase {
   }
 
   /**
+   * Assert that the number of API call is correct.
+   *
+   * @param $increment
+   *   How much the internal counter of API call should be incremented.
+   */
+  protected function assertCallcount($increment = 1) {
+    if ($increment) {
+      self::$apiCallsCount += $increment;
+    }
+
+    $this->assertEquals(self::$apiCallsCount, MailinLog::countApiCalls(), 'API calls count does not match.');
+  }
+
+  /**
    * Create a random folder name that does not exist in Mailin server.
    *
    * @todo what is the method to get all folders?
    */
   protected function createFolderName() {
+    // Do not forget to increment the counter!
+    self::$apiCallsCount++;
+
+    if ($folders = $this->mailinAPI->getFolders()) {
+      $folders = array_map(function ($folder) { return $folder['name']; }, $folders);
+    }
+
     do {
       $name = \MailinTestHelper::randomName();
     }
-    while ($this->findFolder($name));
+    while ($folder && !in_array($name, $folders));
 
     return $name;
+  }
+
+  /**
+   * @covers Mailin\MailinAPI::getFolders
+   */
+  public function testGetFolders() {
+    $folders = $this->mailinAPI->getFolders();
+    $this->assertInternalType('array', $folders, 'Folder fetch operation failed.');
+    $this->assertCallcount();
   }
 
   /**
@@ -64,30 +94,35 @@ class MailinAPITest extends \PHPUnit_Framework_TestCase {
    */
   public function testGetLists() {
     $lists = $this->mailinAPI->getLists();
-    self::$apiCallsCount++;
     $this->assertInternalType('array', $lists, 'List fetch operation failed.');
-    $this->assertEquals(self::$apiCallsCount, MailinLog::countApiCalls(), 'API calls count does not match.');
-  }
-
-  /**
-   * @covers Mailin\MailinAPI::getFolders
-   */
-  public function testGetFolders() {
-    $this->markTestIncomplete('Awaiting API call details to retrieve a list of folders.');
+    $this->assertCallcount();
   }
 
   /**
    * @covers Mailin\MailinAPI::addFolder
-   * @depends testGetLists
+   * @depends testGetFolders
    */
   public function testAddFolder() {
     $folderName = \MailinTestHelper::randomName();
     $folderId = $this->mailinAPI->addFolder($folderName);
-    self::$apiCallsCount++;
 
     $this->assertInternalType('int', $folderId, "Creation of folder $folderName failed.");
-    $this->assertEquals(self::$apiCallsCount, MailinLog::countApiCalls(), 'API calls count does not match.');
+    $this->assertCallcount();
     return array($folderId, $folderName);
+  }
+
+  /**
+   * @covers Mailin\MailinAPI::findFolder
+   * @depends testAddFolder
+   */
+  public function testFindFolder($folder) {
+    list($folderId, $folderName) = $folder;
+    $folder = $this->mailinAPI->findFolder($folderId);
+
+    $this->assertInternalType('array', $folder, 'Find folder operation failed.');
+    $this->assertEquals($folderId, $folder['id'], 'Returned folder has not the expected ID.');
+    $this->assertEquals($folderName, $folder['name'], 'Returned folder has not the expected name.');
+    $this->assertCallcount();
   }
 
   /**
@@ -98,10 +133,9 @@ class MailinAPITest extends \PHPUnit_Framework_TestCase {
     list($folderId, ) = $folder;
     $listName = \MailinTestHelper::randomName();
     $listId = $this->mailinAPI->addList($listName, $folderId);
-    self::$apiCallsCount++;
 
     $this->assertInternalType('int', $listId, "Creation of list $listId failed.");
-    $this->assertEquals(self::$apiCallsCount, MailinLog::countApiCalls(), 'API calls count does not match.');
+    $this->assertCallcount();
     return array($listId, $listName);
   }
 
@@ -111,39 +145,51 @@ class MailinAPITest extends \PHPUnit_Framework_TestCase {
    */
   public function testFindList(array $list) {
     list($listId, $listName) = $list;
+    $list = $this->mailinAPI->findList($listId);
 
-    // Force reset to ensure the lists are up to date, since we just added a new one
-    // then verify that the list is correctly retrieved from the Mailin server.
-    $this->assertTrue($this->mailinAPI->findList($listId, TRUE), "List $listName with ID $listId is missing in the fetched mailing lists.");
-    self::$apiCallsCount++;
-    $this->assertEquals(self::$apiCallsCount, MailinLog::countApiCalls(), 'API calls count does not match.');
-
-    // Without reseting the lists, the API calls number should not increment.
-    $this->assertTrue($this->mailinAPI->findList($listId), "List $listName with ID $listId is missing in the fetched mailing lists.");
-    $this->assertEquals(self::$apiCallsCount, MailinLog::countApiCalls(), 'API calls count does not match.');
+    $this->assertInternalType('array', $list, 'Find list operation failed.');
+    $this->assertEquals($listId, $list['id'], 'Returned list has not the expected ID.');
+    $this->assertEquals($listName, $list['name'], 'Returned list has not the expected name.');
+    $this->assertCallcount();
   }
 
   /**
    * @covers Mailin\MailinAPI::deleteList
    * @depends testAddList
+   * @depends testFindList
    */
   public function testDeleteList(array $list) {
     list($listId, $listName) = $list;
-    $this->assertTrue($this->mailinAPI->deleteList($listName, $listId));
-
-    $this->markTestIncomplete('Awaiting some answers from the Mailin team, because something is broken there...');
+    $this->assertTrue($this->mailinAPI->deleteList($listId), "List $listName with ID $listId was not deleted.");
+    $this->assertCallcount();
 
     // Verify that the list has really been removed from the Mailin server.
-    //$this->assertFalse($this->mailinAPI->findList($listId, TRUE), "List $listName with ID $listId was not removed.");
+    $this->assertFalse($this->mailinAPI->findList($listId), "List $listName with ID $listId was not removed.");
+    self::$apiCallsCount++;
+
+    // Try now to delete a non-existing list.
+    $this->markTestIncomplete('Delete a non-existing list...');
   }
 
   /**
    * @covers Mailin\MailinAPI::deleteList
    * @depends testAddFolder
+   * @depends testFindFolder
    */
   public function testDeleteFolder(array $folder) {
     list($folderId, $folderName) = $folder;
-    $this->assertTrue($this->mailinAPI->deleteFolder($folderName, $folderId));
+    $this->assertTrue($this->mailinAPI->deleteFolder($folderId), "Folder $folderName with ID $folderId was not deleted.");
+    $this->assertCallcount();
+
+    // Verify that the folder has really been removed from the Mailin server.
+    $this->assertFalse($this->mailinAPI->findFolder($folderId), "Folder $folderName with ID $folderId was not removed.");
+    self::$apiCallsCount++;
+
+    // Try now to delete a non-existing folder.
+    $this->markTestIncomplete('Delete a non-existing folder...');
+
+    // Try now to delete a folder which contains several lists.
+    $this->markTestIncomplete('Delete a non-empty folder...');
   }
 
 }
